563023d3aa192fe4d4dc8c0112e430987999a9cc
Jesica606 <jesicaaranibar17@gmail.com>
2023-08-30 15:03:32 -0300

    actividad 4

--- Arrays/array3.js
+++ Arrays/array3.js
@@ -1,4 +1,4 @@
-/*ejercico 3
+/*Ejercicio 3:
 Dada una lista de números, escribe una función en JavaScript que devuelva la suma de todos los números pares en la lista. La función deberá iterar sobre cada número en la lista, comprobar si el número es par y, si es así, añadirlo a la suma total. Usa el bucle que quieras para solucionarlo.*/
 const numeros = [3, 6, 10];
 function sumarPares(numb) {
